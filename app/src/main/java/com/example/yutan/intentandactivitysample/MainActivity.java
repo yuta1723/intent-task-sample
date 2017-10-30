cf4ac3cfde81356c6774f5df3b1b9ded810d2627
yuta1723 <yuta.n1723@gmail.com>
2017-10-30 22:53:17 +0900

    task及びintentの仕様確認のためのコミット

--- app/src/main/java/com/example/yutan/intentandactivitysample/MainActivity.java
+++ app/src/main/java/com/example/yutan/intentandactivitysample/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.yutan.intentandactivitysample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mKeyExtra = "key_extra";
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, String.valueOf(getIntent()));
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClicked");
+                setIntent(new Intent().putExtra(mKeyExtra, "hoge"));
+//                System.exit(0);
+                finish();
+            }
+        });
     }
 }
